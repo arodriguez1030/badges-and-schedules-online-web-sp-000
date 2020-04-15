4ff3b131a94f3d2fa55e56551ad1614fd74121f9
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-15 11:51:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  names =[]
+  array.each do |name|
+    names << "Hello, my name is #{name}."
+  end
+  return names
+end
+
+def assign_rooms(array)
+  assignments= []
+  array.each do |name|
+    assignments << "Hello #{name}! You'll be assigned to room "
+  
